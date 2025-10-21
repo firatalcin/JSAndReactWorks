3d09a2e901d8edbad13bc7e47c66e139382ec644
Fırat Alçın <firatalcin04@gmail.com>
2025-10-21 23:37:17 +0300

    Konu: Degiskenler, Operatorler, Kosul Ifadeleri

---
+++ JS/Bolum-01-JsTemelleri/03-Degisken-Turleri/script.js
@@ -0,0 +1,31 @@
+var urunAdi = "Iphone 13";
+var urunFiyat = 15000;
+
+console.log(typeof urunAdi);
+console.log(typeof urunFiyat);
+
+
+let sayi1 = 10;
+let sayi2 = 20;
+
+console.log(sayi1.toString() + sayi2.toString());
+
+let isim = "fırat";
+let soyad = "alcin";
+
+console.log(isim + " " + soyad);
+
+let sinavNotu = 40;
+
+let basariliMi = (sinavNotu >= 50)
+
+console.log(basariliMi);
+console.log(typeof basariliMi); // boolean
+
+let yas;
+
+
+yas;
+
+console.log(yas);
+console.log(typeof yas);
